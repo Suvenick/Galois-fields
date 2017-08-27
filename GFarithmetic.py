b4b71e1c5a7671d9ba19b18c55bf3362d70aa644
Suvenick <suvenick2@gmail.com>
2017-08-27 22:06:37 +0300

    repair multiplication

--- GFarithmetic.py
+++ GFarithmetic.py
@@ -1,9 +1,8 @@
 import numpy as np
 
-FIELD_POWER ="3" #'32'
+FIELD_POWER ="3" #Field power
 ZERO_POLY = "1011" #'10000000001000000000000000000111' #здесь должен быть не приводимый элемент поля x^32+x^22+x^2+x+1
-number1 = str  #первый элемент поля
-number2 = str  #второй элемент поля
+
 FIELD_POW = {"2": "7", '3': "13", "4": "23", "5": "45", "6": "103", "7": "203", "8": "435", "9": "1021",
              "10": "2011", "11": "4005", "12": "4005", "13": "20033", "14": "42103", "15": "42103", "16": "210013",
              "17": "400011", "18": "1000201", "19": "2000047", "20": "4000011", "21": "10000005", "22": "20000003",
@@ -59,66 +58,44 @@ def add(firstnumber, secondnumber):  # сложение 2-х элементов
     return res  # Возвращает строку
 
 
-def multiply(firstnumb, secondnumb): #умножение двух чисел принимает строки
-    power = int(FIELD_POWER)
-    if firstnumb.find('d') != -1:
-        firstnumb = binary(firstnumb)
-    if secondnumb.find('d') != -1:
-        secondnumb = binary(secondnumb)
 
-    numb1 = np.zeros(power, dtype=int) # создание вспомогательного массива с 0
-    n = 0
-    for var in firstnumb: #заполнение массива 1 числом
-        numb1[n] = int(var)
-        n += 1
-    numb2 = np.zeros(power, dtype=int) # создание вспомогательного массива
-    m = 0
-    for var in secondnumb: #заполнение массива 2 числом
-        numb2[m] = int(var)
-        m += 1
-    solve = np.zeros((power, (power*2)-1), dtype=int) #содание вспомогательной матрицы
-    result = np.zeros((power*2)-1, dtype=int)#создание вспомогательного массива
-
-    i=((power*2)-1)-power
-    z=0
-    b=0
-    for element in numb1:#заполнение матрицы для умножения
-        if element == 1:
-            for elem in numb2:
-                solve[i][b+z] = elem
-                b = b+1
-            i = i-1
-            z = z+1
-        elif element == 0:
-            i = i-1
-            z = z+1
-
-    summ = 0
-    for indx in range((power*2)-1): #считаю значения в каждую ячейку
-        for indx1 in range(power):
-            summ = summ + solve[indx1][indx]
-        result[indx] = summ % 2
-
-    stringres = ''
-    for var in range((power*2)-1):#преобразование массива в строку
-        stringres = stringres+str(result[var])
-
-    if stringres.find('1', 0, power-1) != -1:# проверка на принадлежность поля
-     stringres = add(stringres, ZERO_POLY)# операция сложения с неприводимым полиномом если элемент не в поле
-
-    return stringres #Возвращает строку после проверки на принадлежность полю
+def multiplication(firstnumer, secondnumber):
+    firstnumer = int(firstnumer, 2)
+    secondnumber = secondnumber[::-1]
+    aux = len(secondnumber)
+    auxila = int(ZERO_POLY, 2)
+    i = 0
+    x = 0
+    res = 0
+    while i < aux:
+        if secondnumber[i] == '1':
+            helps = 0
+            if i == 0:
+                res = firstnumer
+            while x < i:
+                firstnumer = firstnumer << 1
+                helps = firstnumer
+                if firstnumer >= 2 ** int(FIELD_POWER):
+                    firstnumer = firstnumer ^ auxila
+                    helps = firstnumer
+                x += 1
+            res = res ^ helps
+        i += 1
+    return bin(res)
 
 
-def inverse_poly(number):#нахождение обратного члена
-    result = multiply(number, number)
-    for i in range(int(FIELD_POWER)-2):
-        result = multiply(number, result)
+def inverse_poly(number):  #нахождение обратного члена
+    result = multiplication(number, number)
+    print("result inverse 1: ", result)
+    for i in range(2 ** int(FIELD_POWER)-4):
+        result = multiplication(number, result)
+        print("number of inverse: ", i+2, " ", result)
     return result
 
 
 def division(number1, number2): #деление
     auxiliary = inverse_poly(number2)
-    result = multiply(number1, auxiliary)
+    result = multiplication(number1, auxiliary)
     return result
 
 
@@ -126,8 +103,6 @@ def subtraction(number1, number2): #вычитание
     auxiliary = inverse_poly(number2)
     result = add(number1, auxiliary)
     return result
-#print(add("d5", "d7"))
-
 
 
 
